577406a3dce93dc8131d3b2260499edced0a0b86
Pippin Williamson <pippin@pippinsplugins.com>
2013-03-07 10:13:22 -0600

    Removed trailing whitespace

--- includes/EDD_C_List_Table.php
+++ includes/EDD_C_List_Table.php
@@ -5,20 +5,20 @@ if( !class_exists( 'WP_List_Table' ) ) {
 }
 
 class EDD_C_List_Table extends WP_List_Table {
-   
-	
+
+
     function __construct(){
         global $status, $page;
-               
+
         //Set parent defaults
         parent::__construct( array(
             'singular'  => 'commission',     //singular name of the listed records
             'plural'    => 'commissions',    //plural name of the listed records
             'ajax'      => false             //does this table support ajax?
         ) );
-       
+
     }
-   
+
 
     function column_default($item, $column_name){
         switch($column_name){
@@ -38,9 +38,9 @@ class EDD_C_List_Table extends WP_List_Table {
                 return print_r($item,true); //Show the whole array for troubleshooting purposes
         }
     }
-   
+
     function column_title($item) {
-       
+
         //Build row actions
         $actions = array();
         $base = admin_url('edit.php?post_type=download&page=edd-commissions');
@@ -51,8 +51,8 @@ class EDD_C_List_Table extends WP_List_Table {
         }
         $actions['edit'] = sprintf('<a href="%s&action=%s&commission=%s">' . __('Edit') . '</a>', $base, 'edit',$item['ID']);
         $actions['delete'] = sprintf('<a href="%s&action=%s&commission=%s">' . __('Delete') . '</a>', $base, 'delete',$item['ID']);
-        
-       
+
+
         $user = get_userdata( $item['user'] );
 
         //Return the title contents
@@ -62,7 +62,7 @@ class EDD_C_List_Table extends WP_List_Table {
             /*$3%s*/ $this->row_actions($actions)
         );
     }
-   
+
     function column_cb($item){
         return sprintf(
             '<input type="checkbox" name="%1$s[]" value="%2$s" />',
@@ -70,7 +70,7 @@ class EDD_C_List_Table extends WP_List_Table {
             /*$2%s*/ $item['ID']
         );
     }
-   
+
 
     function get_columns(){
         $columns = array(
@@ -84,7 +84,7 @@ class EDD_C_List_Table extends WP_List_Table {
         );
         return $columns;
     }
-   
+
     function get_views() {
         $base = admin_url('edit.php?post_type=download&page=edd-commissions');
         $current = isset( $_GET['view'] ) ? $_GET['view'] : '';
@@ -95,7 +95,7 @@ class EDD_C_List_Table extends WP_List_Table {
         );
         return $views;
     }
-   
+
 
     function get_bulk_actions() {
         $actions = array(
@@ -105,10 +105,10 @@ class EDD_C_List_Table extends WP_List_Table {
         );
         return $actions;
     }
-   
+
 
     function process_bulk_action() {
-        
+
         $ids = isset( $_GET['commission'] ) ? $_GET['commission'] : false;
 
         if( !is_array( $ids ) )
@@ -125,7 +125,7 @@ class EDD_C_List_Table extends WP_List_Table {
             if( 'mark_as_unpaid' === $this->current_action() ) {
                 update_post_meta( $id, '_commission_status', 'unpaid' );
             }
-        }       
+        }
     }
 
 
@@ -162,7 +162,7 @@ class EDD_C_List_Table extends WP_List_Table {
 		}
 		return $commissions_data;
     }
-   
+
     /** ************************************************************************
      * @uses $this->_column_headers
      * @uses $this->items
@@ -172,37 +172,37 @@ class EDD_C_List_Table extends WP_List_Table {
      * @uses $this->set_pagination_args()
      **************************************************************************/
     function prepare_items() {
-       
+
         /**
          * First, lets decide how many records per page to show
          */
         $per_page = 20;
-       
-       
+
+
         $columns = $this->get_columns();
         $hidden = array(); // no hidden columns
-       
-       
+
+
         $this->_column_headers = array($columns, $hidden);
-       
+
 
         $this->process_bulk_action();
-       
+
 
         $data = $this->commissions_data();
-   
+
 
         $current_page = $this->get_pagenum();
-    
+
 
         $total_items = count($data);
-       
+
 
         $data = array_slice($data,(($current_page-1)*$per_page),$per_page);
-       
+
 
         $this->items = $data;
-       
+
 
         $this->set_pagination_args( array(
             'total_items' => $total_items,                  //WE have to calculate the total number of items
@@ -210,5 +210,5 @@ class EDD_C_List_Table extends WP_List_Table {
             'total_pages' => ceil($total_items/$per_page)   //WE have to calculate the total number of pages
         ) );
     }
-   
+
 }
