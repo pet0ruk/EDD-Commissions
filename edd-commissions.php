3b16e3c0487ddf78fa3cda4f2d0810e5fd7f5386
Pippin Williamson <pippin@pippinsplugins.com>
2013-03-27 10:32:35 -0500

    Updated v to 1.8.1

--- edd-commissions.php
+++ edd-commissions.php
@@ -6,7 +6,7 @@ Description: Record commisions automatically for users in your site when downloa
 Author: Pippin Williamson
 Author URI: http://pippinsplugins.com
 Contributors: mordauk
-Version: 1.8
+Version: 1.8.1
 */
 
 
@@ -32,7 +32,7 @@ if(!defined('EDDC_PLUGIN_FILE')) {
 
 define( 'EDD_COMISSIONS_STORE_API_URL', 'https://easydigitaldownloads.com' );
 define( 'EDD_COMISSIONS_PRODUCT_NAME', 'Comissions' );
-define( 'EDD_COMISSIONS_VERSION', '1.8' );
+define( 'EDD_COMISSIONS_VERSION', '1.8.1' );
 
 
 /*
