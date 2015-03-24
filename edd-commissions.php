ea0012a09a04f7dd725219584e535ae88a3bb679
fxbenard <fxbenard@gmail.com>
2015-03-24 17:16:00 +0100

    WP Code standards - Ready for i18n - phpcs

--- edd-commissions.php
+++ edd-commissions.php
@@ -7,6 +7,8 @@ Author: Pippin Williamson
 Author URI: http://pippinsplugins.com
 Contributors: mordauk
 Version: 3.1.2
+Text Domain: eddc
+Domain Path: languages
 */
 
 
@@ -17,16 +19,16 @@ Version: 3.1.2
 */
 
 // plugin folder url
-if(!defined('EDDC_PLUGIN_URL')) {
-	define('EDDC_PLUGIN_URL', plugin_dir_url( __FILE__ ));
+if ( ! defined( 'EDDC_PLUGIN_URL' ) ) {
+	define( 'EDDC_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 }
 // plugin folder path
-if(!defined('EDDC_PLUGIN_DIR')) {
-	define('EDDC_PLUGIN_DIR', plugin_dir_path( __FILE__ ));
+if ( ! defined( 'EDDC_PLUGIN_DIR' ) ) {
+	define( 'EDDC_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 }
 // plugin root file
-if(!defined('EDDC_PLUGIN_FILE')) {
-	define('EDDC_PLUGIN_FILE', __FILE__ );
+if ( ! defined( 'EDDC_PLUGIN_FILE' ) ) {
+	define( 'EDDC_PLUGIN_FILE', __FILE__ );
 }
 
 define( 'EDD_COMMISSIONS_VERSION', '3.1.2' );
@@ -41,7 +43,7 @@ define( 'EDD_COMMISSIONS_VERSION', '3.1.2' );
 function eddc_textdomain() {
 	load_plugin_textdomain( 'eddc', false, dirname( plugin_basename( EDDC_PLUGIN_FILE ) ) . '/languages/' );
 }
-add_action('init', 'eddc_textdomain');
+add_action( 'init', 'eddc_textdomain' );
 
 
 function edd_commissions__install() {
@@ -59,27 +61,27 @@ register_activation_hook( __FILE__, 'edd_commissions__install' );
 */
 
 
-include_once(EDDC_PLUGIN_DIR . 'includes/commission-functions.php');
-include_once(EDDC_PLUGIN_DIR . 'includes/email-functions.php');
-include_once(EDDC_PLUGIN_DIR . 'includes/post-type.php');
-include_once(EDDC_PLUGIN_DIR . 'includes/user-meta.php');
-include_once(EDDC_PLUGIN_DIR . 'includes/rest-api.php');
-include_once(EDDC_PLUGIN_DIR . 'includes/short-codes.php');
+include_once EDDC_PLUGIN_DIR . 'includes/commission-functions.php';
+include_once EDDC_PLUGIN_DIR . 'includes/email-functions.php';
+include_once EDDC_PLUGIN_DIR . 'includes/post-type.php';
+include_once EDDC_PLUGIN_DIR . 'includes/user-meta.php';
+include_once EDDC_PLUGIN_DIR . 'includes/rest-api.php';
+include_once EDDC_PLUGIN_DIR . 'includes/short-codes.php';
 
-if( is_admin() ) {
+if ( is_admin() ) {
 
-	if( class_exists( 'EDD_License' ) ) {
+	if ( class_exists( 'EDD_License' ) ) {
 		$eddc_license = new EDD_License( __FILE__, 'Commissions', EDD_COMMISSIONS_VERSION, 'Pippin Williamson' );
 	}
 	//include_once(EDDC_PLUGIN_DIR . 'includes/scheduled-payouts.php');
 	//include_once(EDDC_PLUGIN_DIR . 'includes/masspay/class-paypal-masspay.php');
-	include_once(EDDC_PLUGIN_DIR . 'includes/reports.php');
-	include_once(EDDC_PLUGIN_DIR . 'includes/settings.php');
-	include_once(EDDC_PLUGIN_DIR . 'includes/admin/list.php');
-	include_once(EDDC_PLUGIN_DIR . 'includes/admin/metabox.php');
-	include_once(EDDC_PLUGIN_DIR . 'includes/admin/widgets.php');
-	include_once(EDDC_PLUGIN_DIR . 'includes/admin/upgrades.php');
-	include_once(EDDC_PLUGIN_DIR . 'includes/EDD_C_List_Table.php');
+	include_once EDDC_PLUGIN_DIR . 'includes/reports.php';
+	include_once EDDC_PLUGIN_DIR . 'includes/settings.php';
+	include_once EDDC_PLUGIN_DIR . 'includes/admin/list.php';
+	include_once EDDC_PLUGIN_DIR . 'includes/admin/metabox.php';
+	include_once EDDC_PLUGIN_DIR . 'includes/admin/widgets.php';
+	include_once EDDC_PLUGIN_DIR . 'includes/admin/upgrades.php';
+	include_once EDDC_PLUGIN_DIR . 'includes/EDD_C_List_Table.php';
 } else {
-	include_once(EDDC_PLUGIN_DIR . 'includes/adaptive-payments.php');
+	include_once EDDC_PLUGIN_DIR . 'includes/adaptive-payments.php';
 }
