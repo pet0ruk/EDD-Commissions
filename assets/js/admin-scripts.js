c03df8e5f02b14a83d5723719bebd191a4b774a0
Chris Klosowski <cklosowski@gmail.com>
2015-07-06 14:43:34 -0700

    Batch processing of payout file for #44

---
+++ assets/js/admin-scripts.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function($) {
+	$('.eddc-commissions-export-toggle').click( function() {
+		$('.eddc-commissions-export-toggle').toggle();
+		$('#eddc-export-commissions').toggle();
+	});
+});
